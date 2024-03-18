ccb8e89416804897c595e87482f7e9c651b63765
sanyagvozdoder <alexshadrov1712@gmail.com>
2024-03-18 19:50:26 +0700

    test commit

---
+++ algoritms/sidebarscripts.js
@@ -0,0 +1,6 @@
+function openSideBar(){
+    document.getElementsByClassName('side-bar').style.width = '250px'
+}
+function closeSideBar(){
+    document.getElementsByClassName('side-bar').style.width = '0px'
+}
