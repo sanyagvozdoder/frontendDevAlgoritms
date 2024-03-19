1b4f718cfc78d00d68b270197001d3c2ad484af9
sanyagvozdoder <alexshadrov1712@gmail.com>
2024-03-19 15:44:24 +0700

    last super caif change - js todo

--- algoritms/js/sideBarScripts.js
+++ algoritms/js/sideBarScripts.js
@@ -1,12 +1,12 @@
-//TODO:add opacity to main colors
-
 function openSideBar(){
     document.getElementById('side-nav').style.width = "275px"
     document.getElementById('main-container').style.marginLeft = "275px"
+    document.getElementById('main-container').style.opacity = "0.4"
 }
 function closeSideBar(){
     document.getElementById('side-nav').style.width = "0px"
     document.getElementById('main-container').style.marginLeft = "210px"
+    document.getElementById('main-container').style.opacity = "1"
 
 }
 
