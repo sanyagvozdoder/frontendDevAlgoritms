436a663fd6f8b2c5cee0750bb88fa7e4abe55c2b
BalaenicepsRex473 <glebdashcow@gmail.com>
2024-04-13 15:28:46 +0300

    raw unworking ant colony optimization

--- static/js/algorithms/clusterization.js
+++ static/js/algorithms/clusterization.js
@@ -2,71 +2,59 @@ const canvas = document.getElementById('paintCanvas');
 const context = canvas.getContext('2d');
 let points = [];
 
-function distance(pointA, pointB)
-{
+function distance(pointA, pointB) {
     const dx = pointA.x - pointB.x;
     const dy = pointA.y - pointB.y;
     return Math.sqrt(dx * dx + dy * dy);
 }
 
-function draw(event)
-{
+function draw(event) {
     const x = event.clientX - canvas.offsetLeft;
     const y = event.clientY - canvas.offsetTop;
     points.push({ x, y });
-    
+
     context.fillStyle = "black";
     context.fillRect(x, y, 15, 15);
 
 }
 canvas.addEventListener('click', draw);
 
-function clear(context)
-{
+function clear(context) {
     points = []
     context.fillStyle = "white"
     context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 
-function chooseCentroids(points, amount)
-{
+function chooseCentroids(points, amount) {
     const centroids = [];
-    for (let i = 0; i < amount; i++)
-    {
+    for (let i = 0; i < amount; i++) {
         let indx = Math.floor(Math.random() * points.length);
-        while (centroids.includes(points[indx]))
-        {
+        while (centroids.includes(points[indx])) {
             indx = Math.floor(Math.random() * points.length);
         }
-        centroids.push(points[indx]);   
+        centroids.push(points[indx]);
     }
     return centroids;
 }
 
-function kMeanClusterization(points, amount)
-{
+function kMeanClusterization(points, amount) {
     let centroids = chooseCentroids(points, amount)
     let clusters = [];
 
-    for(let i = 0; i < amount; ++i)
-    {
+    for (let i = 0; i < amount; ++i) {
         clusters.push([])
     }
 
-    for (let i = 0; i < 100 ; i++)
-    {
-        for (let k = 0; k < amount; k++)
-        {
-            clusters[k] = [];    
+    for (let i = 0; i < 100; i++) {
+        for (let k = 0; k < amount; k++) {
+            clusters[k] = [];
         }
-        
-        for (let j = 0; j < points.length; j++)
-        {
+
+        for (let j = 0; j < points.length; j++) {
             let minDistance = Infinity;
             let closestCluster = 0;
-            for (let n = 0; n < amount; n++)
-             {
+            for (let n = 0; n < amount; n++) {
                 const dist = distance(centroids[n], points[j]);
 
                 if (dist < minDistance) {
@@ -78,20 +66,20 @@ function kMeanClusterization(points, amount)
         }
 
         let newCentroids = [];
-        for (let m = 0; m < amount; m++)
-        {
-            if (clusters[m].length > 0) 
-            {
+        for (let m = 0; m < amount; m++) {
+            if (clusters[m].length > 0) {
                 const sumX = clusters[m].reduce((acc, point) => acc + point.x, 0);
                 const sumY = clusters[m].reduce((acc, point) => acc + point.y, 0);
                 const centroidX = sumX / clusters[m].length;
                 const centroidY = sumY / clusters[m].length;
                 newCentroids.push({ x: centroidX, y: centroidY });
             }
-            else 
-            {
+            else {
                 newCentroids.push(centroids[m]);
             }
+        }
+        if (centroids[0] == newCentroids[0] & centroids[1] == newCentroids[1] & centroids[1] == newCentroids[1]) {
+            break;
         }
 
         centroids = newCentroids;
@@ -100,47 +88,38 @@ function kMeanClusterization(points, amount)
     return clusters;
 }
 
-function kMeanClusterizationOnCanvas(points, context)
-{
+function kMeanClusterizationOnCanvas(points, context) {
 
-    if (points.length < 3)
-    {
+    if (points.length < 3) {
         alert("Вы ввели меньше 3 точек");
         return;
     }
-    
+
     let clusters = kMeanClusterization(points, 3);
 
 
-    for (let i = 0; i < clusters[0].length; i++)
-    {
+    for (let i = 0; i < clusters[0].length; i++) {
         context.fillStyle = 'orange';
         context.fillRect(clusters[0][i].x, clusters[0][i].y, 15, 5);
     }
-    for (let i = 0; i < clusters[1].length; i++)
-    {
+    for (let i = 0; i < clusters[1].length; i++) {
         context.fillStyle = 'red';
-        context.fillRect(clusters[1][i].x, clusters[1][i].y,  15, 5);
+        context.fillRect(clusters[1][i].x, clusters[1][i].y, 15, 5);
     }
-    for (let i = 0; i < clusters[2].length; i++)
-    {
+    for (let i = 0; i < clusters[2].length; i++) {
         context.fillStyle = 'green';
-        context.fillRect(clusters[2][i].x, clusters[2][i].y,  15, 5);
+        context.fillRect(clusters[2][i].x, clusters[2][i].y, 15, 5);
     }
 }
 
-function hierarchicalClusterization(points, amount)
-{
+function hierarchicalClusterization(points, amount) {
     let clusters = points.map(points => [points]);
-    while (clusters.length != amount) 
-    {
+    while (clusters.length != amount) {
         let dist = [];
-        for (let i = 0; i < clusters.length; i++)
-        {
-            for (let j = i + 1; j < clusters.length; j++)
-            {
+        for (let i = 0; i < clusters.length; i++) {
+            for (let j = i + 1; j < clusters.length; j++) {
                 let distanciya = Math.min(...clusters[i].map(pointA => Math.min(...clusters[j].map(pointB => distance(pointA, pointB)))));
-                dist.push({i, j, distanciya});
+                dist.push({ i, j, distanciya });
             }
         }
 
@@ -158,76 +137,61 @@ function hierarchicalClusterization(points, amount)
     return clusters;
 }
 
-function hierarchicalClusterizationOnCanvas(points, context)
-{
-    if (points.length < 3)
-    {
+function hierarchicalClusterizationOnCanvas(points, context) {
+    if (points.length < 3) {
         alert("Вы ввели меньше 3 точек")
         return
     }
-    
+
     let clusters = hierarchicalClusterization(points, 3);
 
 
-    for (let i = 0; i < clusters[0].length; i++)
-    {
+    for (let i = 0; i < clusters[0].length; i++) {
         context.fillStyle = 'blue';
-        context.fillRect(clusters[0][i].x, clusters[0][i].y + 5,  15, 5);
+        context.fillRect(clusters[0][i].x, clusters[0][i].y + 5, 15, 5);
     }
-    for (let i = 0; i < clusters[1].length; i++)
-    {
+    for (let i = 0; i < clusters[1].length; i++) {
         context.fillStyle = 'yellow';
-        context.fillRect(clusters[1][i].x, clusters[1][i].y + 5,  15, 5);
+        context.fillRect(clusters[1][i].x, clusters[1][i].y + 5, 15, 5);
     }
-    for (let i = 0; i < clusters[2].length; i++)
-    {
+    for (let i = 0; i < clusters[2].length; i++) {
         context.fillStyle = 'pink';
-        context.fillRect(clusters[2][i].x, clusters[2][i].y + 5,  15, 5);
+        context.fillRect(clusters[2][i].x, clusters[2][i].y + 5, 15, 5);
     }
 }
 
-function chooseMedoids(points, amount)
-{
+function chooseMedoids(points, amount) {
     const medoids = [];
-    for (let i = 0; i < amount; i++)
-    {
+    for (let i = 0; i < amount; i++) {
         let indx = Math.floor(Math.random() * points.length);
-        while (medoids.includes(points[indx]))
-        {
+        while (medoids.includes(points[indx])) {
             indx = Math.floor(Math.random() * points.length)
         }
-        medoids.push(points[indx]);   
+        medoids.push(points[indx]);
     }
     return medoids;
 }
 
-function kMedoidsClusterization(points, amount)
-{
+function kMedoidsClusterization(points, amount) {
     let medoids = chooseMedoids(points, amount)
     let clusters = [];
 
-    for(let i = 0; i < amount; ++i)
-    {
+    for (let i = 0; i < amount; ++i) {
         clusters.push([])
     }
 
-    for (let i = 0; i < 100 ; i++)
-    {
-        for (let k = 0; k < amount; k++)
-        {
-            clusters[k] = [];    
+    for (let i = 0; i < 100; i++) {
+        for (let k = 0; k < amount; k++) {
+            clusters[k] = [];
         }
-        
-        for (let j = 0; j < points.length; j++)
-        {
+
+        for (let j = 0; j < points.length; j++) {
             let minDistance = Infinity;
             let closestCluster = 0;
-            for (let n = 0; n < amount; n++)
-             {
+            for (let n = 0; n < amount; n++) {
                 const dist = distance(medoids[n], points[j]);
 
-                if (dist < minDistance)
-                 {
+                if (dist < minDistance) {
                     minDistance = dist;
                     closestCluster = n;
                 }
@@ -236,23 +200,23 @@ function kMedoidsClusterization(points, amount)
         }
 
         let newMedoids = [];
-        for (let m = 0; m < amount; m++)
-        {
+        for (let m = 0; m < amount; m++) {
             const cluster = clusters[m];
             let minDist = Infinity;
             let newMedoid = 0;
 
-            for (let k = 0; k < cluster.length; k++)
-            {
+            for (let k = 0; k < cluster.length; k++) {
                 const potential = cluster[k];
                 const dist = cluster.reduce((acc, point) => acc + distance(point, potential), 0);
-                if (dist < minDist)
-                {
+                if (dist < minDist) {
                     minDist = dist;
                     newMedoid = potential;
                 }
             }
             newMedoids.push(newMedoid);
+        }
+        if (medoids[0] == newMedoids[0] & medoids[1] == newMedoids[1] & medoids[1] == newMedoids[1]) {
+            break;
         }
 
         medoids = newMedoids;
@@ -261,37 +225,32 @@ function kMedoidsClusterization(points, amount)
     return clusters;
 }
 
-function kMedoidsClusterizationOnCanvas(points, context)
-{
-    if (points.length < 3)
-    {
+function kMedoidsClusterizationOnCanvas(points, context) {
+    if (points.length < 3) {
         alert("Вы ввели меньше 3 точек")
         return
     }
-    
+
     let clusters = kMedoidsClusterization(points, 3);
 
 
-    for (let i = 0; i < clusters[0].length; i++)
-    {
+    for (let i = 0; i < clusters[0].length; i++) {
         context.fillStyle = '#FFE4E1';
-        context.fillRect(clusters[0][i].x, clusters[0][i].y + 10,  15, 5);
+        context.fillRect(clusters[0][i].x, clusters[0][i].y + 10, 15, 5);
     }
-    for (let i = 0; i < clusters[1].length; i++)
-    {
+    for (let i = 0; i < clusters[1].length; i++) {
         context.fillStyle = '#00FFFF';
-        context.fillRect(clusters[1][i].x, clusters[1][i].y + 10,  15, 5);
+        context.fillRect(clusters[1][i].x, clusters[1][i].y + 10, 15, 5);
     }
-    for (let i = 0; i < clusters[2].length; i++)
-    {
+    for (let i = 0; i < clusters[2].length; i++) {
         context.fillStyle = '#FF00FF';
         context.fillRect(clusters[2][i].x, clusters[2][i].y + 10, 15, 5);
     }
 }
 
 
-document.getElementById("kMeansActivate").addEventListener('click', ()=>{kMeanClusterizationOnCanvas(points, context)});
-document.getElementById("hierarchicalActivate").addEventListener('click', ()=>{hierarchicalClusterizationOnCanvas(points, context)});
-document.getElementById("kMedoidsActivate").addEventListener('click', ()=>{kMedoidsClusterizationOnCanvas(points, context)});
+document.getElementById("kMeansActivate").addEventListener('click', () => { kMeanClusterizationOnCanvas(points, context) });
+document.getElementById("hierarchicalActivate").addEventListener('click', () => { hierarchicalClusterizationOnCanvas(points, context) });
+document.getElementById("kMedoidsActivate").addEventListener('click', () => { kMedoidsClusterizationOnCanvas(points, context) });
 
 document.getElementById("goslingCSS").addEventListener('click', () => { clear(context) });
